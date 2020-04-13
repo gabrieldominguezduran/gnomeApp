67a302944b47d3484364149b3ea864651a249c2e
Gabriel Dominguez <gdominguez@synergie.es>
2020-04-13 22:01:33 +0200

    Updates

---
+++ precache-manifest.5214f617331f268a41ec054627bdce69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a397dcde1757fb7ff019792697b7ce47",
+    "url": "/gnomeApp/index.html"
+  },
+  {
+    "revision": "3e8be7eb7463bbcb71d2",
+    "url": "/gnomeApp/static/css/main.cb31d72d.chunk.css"
+  },
+  {
+    "revision": "6d3f7b51f87ab8b17778",
+    "url": "/gnomeApp/static/js/2.8ec6ff71.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gnomeApp/static/js/2.8ec6ff71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e8be7eb7463bbcb71d2",
+    "url": "/gnomeApp/static/js/main.7a9be1cd.chunk.js"
+  },
+  {
+    "revision": "23350dcc55b7ae7353bf",
+    "url": "/gnomeApp/static/js/runtime-main.847a4265.js"
+  },
+  {
+    "revision": "77b81eb8da5002e240fab4b0dc85ed2f",
+    "url": "/gnomeApp/static/media/main.77b81eb8.jpg"
+  }
+]);
